2d38a733ff441e188f4b36a4a45916da0bb6739f
skgitacc <sivakumarmuthu9797@gmail.com>
2022-02-19 19:30:12 +0530

    push

---
+++ src/org/com/Sel.java
@@ -0,0 +1,24 @@
+package org.com;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sel {
+	public static void main(String[] args) throws IOException {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\admin\\eclipse-workspace\\Seliniumss\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demo.automationtesting.in/Alerts.html");
+		driver.manage().window().maximize();
+
+		TakesScreenshot ss = (TakesScreenshot) driver;
+		File src = ss.getScreenshotAs(OutputType.FILE);
+
+		File dest = new File("\\C:\\Users\\admin\\Pictures\\Camera Roll\\sk.png");
+	}
+}
